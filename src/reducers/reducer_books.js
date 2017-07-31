5a106aab08dbe11a64b797697274ecf0b167887d
selliott <selliott@edmunds.com>
2017-07-31 11:48:55 -0700

    udemy redux books class

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function () {
+    return [
+        {
+            title: 'Javascript: The good parts',
+            pages: 101
+        },
+        {
+            title: 'Harry Potter',
+            pages: 39
+        },
+        {
+            title: 'The Dark Tower',
+            pages: 89
+        },
+        {
+            title: 'Eloquent Ruby',
+            pages: 1
+        }
+    ];
+}
